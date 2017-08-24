be8896efee208efc2a924d7a7c226c82d48eb4d4
Andrew Craig <andrew.craig@itnetx.ch>
2017-08-24 07:40:54 +0200

    update ee

--- SpotifyEmailParseBody/run.ps1
+++ SpotifyEmailParseBody/run.ps1
@@ -2,11 +2,17 @@
   write-host $_.exception.message
 }
 
+write-verbose "getting input" -Verbose
+
 $RequestBody = (get-content -raw $request | convertfrom-json)
+
+write-verbose "splitting input" -Verbose
 
 $RequestBody = $RequestBody -split '\\r\\n'
 
 $RequestBody = $RequestBody | ? { $_ -ne "" }
+
+write-verbose "getting artist by regex" -Verbose
 
 $artist = $RequestBody -imatch 'Artist:?=? ?(.+)'
 if($artist){
@@ -14,11 +20,15 @@ if($artist){
   $Artist = $matches[1]
 }
 
+write-verbose "getting track by regex" -Verbose
+
 $track = $RequestBody -match 'track:?=? ?(.+)'
 if($track){
   $track -imatch 'track:?=? ?(.+)' | out-null
   $track = $matches[1]
 }
+
+write-verbose "checking in case artist or track wasn't specified" -Verbose
 
 if($RequestBody.count -eq 2 -and [string]::IsNullOrEmpty($artist) -and [string]::IsNullOrEmpty($track)){
   $artist = $RequestBody[0]
@@ -29,10 +39,14 @@ if($RequestBody.count -eq 1 -and [string]::IsNullOrEmpty($track)){
   $track = $RequestBody[0]
 }
 
+
+write-verbose "building filter" -Verbose
 $filter = "name:$track"
 
 if ([string]::IsNullOrEmpty($Artist) -eq $false) {
     $filter += " artist:$artist"
 }
+
+write-verbose "writing output" -Verbose
 
 @{filter = $filter; functionkey = $REQ_QUERY_CODE} | convertto-json | Out-File -FilePath $queue -Encoding utf8
